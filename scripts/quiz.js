aaebc20a95996a301f63ccbb4197399faf6da833
Mindy McAdams <mmcadams@gmail.com>
2015-03-08 11:20:19 -0400

    revise quiz files

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,22 +1,22 @@
 // adapted from 
 // http://www.javascriptsource.com/miscellaneous/basic-javascript-quiz.html
 
-// Insert your number of questions
+// insert your number of questions
 var numQues = 4;
 
-// Insert your number of choices in each question
+// insert your number of choices in each question
 var numChoi = 3;
 
 var answers = new Array();
 var answerstext = new Array();
 
-// Insert values of correct answers - be careful about the order 
+// insert values of correct answers - be careful about the order 
 answers.push("cascading");
 answers.push("ul_ol_dl");
 answers.push("nav");
 answers.push("carpet");
 
-// Insert correct answers to questions - be careful about the order 
+// insert correct answers to questions - be careful about the order 
 answerstext.push("Cascading Style Sheets");
 answerstext.push("UL, OL, DL");
 answerstext.push("nav, article, footer, section");
@@ -27,9 +27,9 @@ function getScore(form) {
   var score = 0;
   var currElt; // current element 
   var currSelection;
-  for (i=0; i<numQues; i++) {
-    currElt = i*numChoi;
-    for (j=0; j<numChoi; j++) {
+  for (i=0; i < numQues; i++) {
+    currElt = i * numChoi;
+    for (j=0; j < numChoi; j++) {
       currSelection = form.elements[currElt + j];
       if (currSelection.checked) {
         if (currSelection.value == answers[i]) {
@@ -43,7 +43,7 @@ function getScore(form) {
   form.percentage.value = score + "%";
   
   var correctAnswers = "";
-  for (i=1; i<=numQues; i++) {
+  for (i=1; i <= numQues; i++) {
     correctAnswers += i + ". " + answerstext[i-1] + "\n";
   }
   form.solutions.value = correctAnswers;
